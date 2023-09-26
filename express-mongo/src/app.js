82c3a26d186992eaba2acf938cf03e82d9a26163
leandroaugust0 <leandroaugusto9999@gmail.com>
2023-09-25 21:31:58 -0300

    Tópico finalizado

---
+++ express-mongo/src/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import conectaNaDataBase from "./config/dbConnect.js";
+import routes from "./routes/index.js";
+
+const conexao = await conectaNaDataBase();
+
+conexao.on("error", (erro) => {
+  console.error("erro de conexão", erro);
+});
+
+conexao.once("open", () => {
+  console.log("Conexão com o banco feita com sucesso");
+});
+
+const app = express();
+routes(app);
+
+
+// app.put("/livros/:id", (req, res) => {
+//   const index = buscaLivro(req.params.id);
+//   livros[index].titulo = req.body.titulo;
+//   res.status(200).json(livros);
+// });
+
+// app.delete("/livros/:id", (req, res) => {
+//   const index = buscaLivro(req.params.id);
+//   livros.splice(index, 1);
+//   res.status(200).send("livro removido com sucesso");
+// });
+
+export default app;
